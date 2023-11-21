6a62dc64b16f334690f1bf5fea4458aecd39e263
Alfredo Moralejo <amoralej@redhat.com>
2023-11-21 14:08:31 +0100

    Fix metallb operator deployment

--- install_external_operators/metallb/install.sh
+++ install_external_operators/metallb/install.sh
@@ -1,5 +1,9 @@
+oc create -f namespace.yaml
 oc create -f operatorhub-catalog.yml
+oc create -f metallb-operatorgroup.yaml
 oc create -f metallb-operator.yaml
 oc create -f privileged-role-binding.yaml
 oc apply -f patch-deployment-controller-manager.yaml
 oc apply -f patch-deployment-webhook-server.yaml
+sleep 60
+oc create -f metallb.yaml
